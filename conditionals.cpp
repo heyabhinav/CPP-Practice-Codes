d004a5ffeafca05b559a35c8169698d1eb37d0f7
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2022-03-30 14:25:27 +0530

    Takes input in one line and added return statement in main function.

--- conditionals.cpp
+++ conditionals.cpp
@@ -1,42 +1,21 @@
-// To check whether number is positive or not!
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int num;
-//     cout<<"Enter a Number";
-//     cin>>num;
-    
-//     if (num>0){
-//         cout<<"Number is positive";
-//     }
-//     else if (num<0){
-//         cout<<"Number is negative";
-//     }
-//     else{
-//         cout<<"Number is Zero";
-//     }
-// }
-
 // To Print maximum of two numbers!
 #include <iostream>
 using namespace std;
 int main(){
-    int first;
-    cout<<"Enter first number";
-    cin>>first;
-    int second;
-    cout<<"Enter second number";
-    cin>>second;
-
-    if (first>second){
-        cout<<first<<"is greater";
+    int first_number, second_number;
+    cout << "Enter two numbers separated by space : " << endl;
+    cin >> first_number >> second_number;
+    
+    if (first_number > second_number){
+        cout << "First number is greater than second number.";
     }
-    else if(second>first){
-        cout<<second<<"is greater";
+    else if(second_number > first_number){
+        cout << "Second number is greater than first number.";
     }
     else{
-        cout<<"Both are equal";
+        cout << "Both numbers are equal.";
     }
+    return 0;
 }
 
 
